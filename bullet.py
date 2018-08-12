262ef0462119410131e007a638b61b45b5bd65a4
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2018-08-12 22:55:15 +0545

    Adds bullet sprite and cannon rotation

--- bullet.py
+++ bullet.py
@@ -5,7 +5,8 @@ import math
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, start_x, start_y, end_x, end_y):
         super(Bullet, self).__init__()
-        self.image = pygame.image.load("sprites/cannon.png")
+        self.image = pygame.image.load("sprites/bullet.png")
+        self.image = pygame.transform.scale(self.image, (30, 30))
         self.rect = self.image.get_rect(x=start_x, y=start_y)
         self.timer = 0.0
         self.angle = 0
--- cannon.py
+++ cannon.py
@@ -7,14 +7,17 @@ from bullet import Bullet
 class Cannon(pygame.sprite.Sprite):
     def __init__(self, start_x, start_y):
         super(Cannon, self).__init__()
-        self.image = pygame.image.load("sprites/cannon.png")
+        self.cannon_image = pygame.image.load("sprites/cannon.png")
+        self.image = self.cannon_image
         self.rect = self.image.get_rect(x=start_x, y=start_y)
         self.timer = 0
         self.angle = 0
 
     def update(self, soldier, bullet_group):
+        angle = self.angle - math.atan2(soldier.rect.y - self.rect.y, soldier.rect.x - self.rect.x)
+        self.image = pygame.transform.rotate(self.cannon_image, math.degrees(angle) + 180)
         if self.timer == 0:
-            bullet = Bullet(self.rect.x, self.rect.y, soldier.rect.x, soldier.rect.y)
+            bullet = Bullet(self.rect.x, self.rect.y + 25, soldier.rect.centerx, soldier.rect.centery)
             bullet_group.add(bullet)
         self.timer += 1
         if self.timer == 100:
